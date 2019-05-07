ee5a276d9a20565dbbf13759a5ae158252334f8c
birgitta run <birgittarun95@gmail.com>
2019-05-07 09:55:45 +0000

    breytti ð í d

--- castleapartments/castleapartments/settings.py
+++ castleapartments/castleapartments/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'eignir.apps.EignirConfig',
-    'verðmat.apps.VerðmatConfig',
+    'verdmat.apps.VerdmatConfig',
 ]
 
 MIDDLEWARE = [
@@ -78,8 +78,6 @@ WSGI_APPLICATION = 'castleapartments.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'axdsnirv',
         'USER': 'axdsnirv',
