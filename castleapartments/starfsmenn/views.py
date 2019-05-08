2bfdb2c3bd5c3e46277af308a9a90bcfc936f6f9
Sigurgeir <sigurgeir16@ru.is>
2019-05-08 13:10:46 +0000

    Some work on starfsmenn page

--- castleapartments/starfsmenn/views.py
+++ castleapartments/starfsmenn/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
 
+dummies = [
+    {
+        "name": "J Json",
+        "license": "Löggiltur Fasteignarsali",
+        "contactInfo": "dummy@dummies.com",
+        "description": "I did stuff before i sold houses"
+    },
+
+    {
+        "name": "Brainan",
+        "license": "Löggiltur Fasteignarsali",
+        "contactInfo": "dummy@dummies.com",
+        "description": "Houses aint the only thing i sell ;)"
+    },
+
+    {
+        "name": "Brainina",
+        "license": "Löggiltur Fasteignarsali",
+        "contactInfo": "dummy@dummies.com",
+        "description": "This is just temporary..."
+    },
+
+    {
+        "name": "Samus Aran",
+        "license": "Löggiltur Fasteignarsali",
+        "contactInfo": "dummy@dummies.com",
+        "description": "My parents wanted a girl... and liked metroid"
+    },
+]
 
 def index(request):
-    return render(request, 'starfsmenn/index.html')
+    context = {"dummies": dummies}
+    return render(request, 'starfsmenn/index.html', context)
+
