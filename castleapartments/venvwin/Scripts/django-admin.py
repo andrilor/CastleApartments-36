25b474a1d8b6ea33db43516db73c71addf7600ff
Fannar <fannarleo95@hotmail.com>
2019-05-08 13:50:09 +0000

    dno

---
+++ castleapartments/venvwin/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\fanna\OneDrive\Documents\Skóli\Verklegt námskeið 2\Git_branch\CastleApartments-36\castleapartments\venvwin\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
