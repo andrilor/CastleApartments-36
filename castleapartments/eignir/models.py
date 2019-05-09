3bb1590ec4d64e8787dc14de010cf1f21817435f
andrilor <Andri.lorenz@gmail.com>
2019-05-09 15:57:46 +0000

    db fyrir models starfsmenn og eignir kommið tables kominn og búinn að populata það

--- castleapartments/eignir/models.py
+++ castleapartments/eignir/models.py
@@ -2,46 +2,50 @@ from django.db import models
 from starfsmenn.models import Starfsmenn
 
 class GPS_Stadsetning(models.Model):
-    gotuheiti = models.CharField(max_length=255)
+    heimilisfang = models.CharField(max_length=255)
     lat = models.FloatField(max_length=(10, 6))
     lng = models.FloatField(max_length=(10, 6))
+    def __str__(self):
+        return self.lat and self.lat
 
 class Eign(models.Model):
-    Address = models.CharField(max_length=255)
-    Town_name = models.CharField(max_length=255)
-    zip_code = models.IntegerField()
-    Price = models.IntegerField()
-    Brunabotamat = models.IntegerField()
-    Fasteignamat = models.IntegerField()
-    Type = models.CharField(max_length=255)
-    Description = models.CharField(max_length=999, blank=True)
-    Size = models.FloatField()
-    Building_year = models.DateField()
-    Number_of_rooms = models.IntegerField()
-    Number_of_bathrooms = models.IntegerField()
-    Number_of_bedrooms = models.IntegerField()
-    put_on_sale = models.DateTimeField()
-    Available_now = models.BooleanField()
-    Housed_drawing = models.BooleanField()
-    private_entrance = models.BooleanField()
-    public_entrance = models.BooleanField()
-    Sundeck = models.BooleanField()
-    Balcony = models.BooleanField()
-    Garden = models.BooleanField()
-    Laundry_room = models.BooleanField()
-    wheelchair_Access = models.BooleanField()
-    Elevator = models.BooleanField()
-    Garage = models.BooleanField()
-    Parking_Space = models.BooleanField()
-
-    Seller_Name = models.CharField(max_length=255)
-    Seller_Phone = models.IntegerField()
-    Seller_Email = models.CharField(max_length=255)
+    heimilisfang = models.CharField(max_length=255)
+    baejarfelag = models.CharField(max_length=255)
+    postnumer = models.IntegerField()
+    verd = models.IntegerField()
+    brunabotamat = models.IntegerField()
+    fasteignamat = models.IntegerField()
+    tegund = models.CharField(max_length=255)
+    lysing = models.CharField(max_length=999, blank=True)
+    staerd = models.IntegerField()
+    byggingarar = models.DateField()
+    fjoldi_herbergja = models.IntegerField()
+    fjoldi_badherberga = models.IntegerField()
+    fjoldi_svefnherberga = models.IntegerField()
+    sett_a_solu = models.DateField()
+    laust_nuna = models.BooleanField()
+    teikning = models.BooleanField()
+    serinngangur = models.BooleanField()
+    sameigninlegur_inngangur = models.BooleanField()
+    solpallur = models.BooleanField()
+    svalir = models.BooleanField()
+    gardur = models.BooleanField()
+    thvottahus = models.BooleanField()
+    hjolastolaadgengi = models.BooleanField()
+    lyfta = models.BooleanField()
+    bilskur = models.BooleanField()
+    bilastaedi = models.BooleanField()
+    nafn_seljanda = models.CharField(max_length=255)
+    simi_seljanda = models.IntegerField()
+    netfang_seljanda = models.CharField(max_length=255)
     Starfsmenn = models.ForeignKey(Starfsmenn, on_delete=models.CASCADE)
-    GPS_Stadsetning = models.ForeignKey(GPS_Stadsetning, on_delete=models.CASCADE)
+    gps_stadsetning = models.ForeignKey(GPS_Stadsetning, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.heimilisfang
 
-class EignImage(models.Model):
-    image = models.CharField(max_length=999)
-    Eign = models.ForeignKey(Eign, on_delete=models.CASCADE)
-
+class Eignmynd(models.Model):
+    mynd = models.CharField(max_length=999)
+    eign = models.ForeignKey(Eign, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.mynd
 
--- castleapartments/opinhus/models.py
+++ castleapartments/opinhus/models.py
@@ -1,10 +1,3 @@
 from django.db import models
-from castleapartments.eignir.models import Eignir
 
-
-class opinhus(models.Model):
-    Start = models.DateTimeField()
-    End = models.DateTimeField()
-    eignir = models.ForeignKey(Eignir, on_delete=models.CASCADE)
-
-
+#dfadf
