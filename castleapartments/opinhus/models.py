bdf3bdddcbea008ef9d1f2e8bb9a5a932ffd510b
andrilor <Andri.lorenz@gmail.com>
2019-05-09 16:19:50 +0000

    models

--- castleapartments/opinhus/models.py
+++ castleapartments/opinhus/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from eignir.models import Eignir
 
-#dfadf
+class opinhus(models.Model):
+    Start = models.DateTimeField()
+    End = models.DateTimeField()
+    eignir = models.ForeignKey(Eignir, on_delete=models.CASCADE)
