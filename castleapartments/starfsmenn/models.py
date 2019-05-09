5c5c27619f519c60a8ef1223429bccbcd54b4fb4
vianey17 <vianey17@ru.is>
2019-05-09 02:11:52 +0000

    starfsmenn templates

--- castleapartments/starfsmenn/models.py
+++ castleapartments/starfsmenn/models.py
@@ -9,7 +9,11 @@ class Starfsmadur(models.Model):
     email = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=999, blank=True)
+    def __str__(self):
+        return self.name
 
 class StarfsmadurImage(models.Model):
     image = models.CharField(max_length=999)
     starfsmadur = models.ForeignKey(Starfsmadur, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.image
