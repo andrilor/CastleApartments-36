d98742ec0206814059e0c47e227fb275227ce565
vianey17 <vianey17@ru.is>
2019-05-09 00:18:55 +0000

    Starfsmadur Model

--- castleapartments/starfsmenn/models.py
+++ castleapartments/starfsmenn/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Starfsmadur(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.IntegerField()
+    email = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=999, blank=True)
+
+class StarfsmadurImage(models.Model):
+    image = models.CharField(max_length=999)
+    starfsmadur = models.ForeignKey(Starfsmadur, on_delete=models.CASCADE)
