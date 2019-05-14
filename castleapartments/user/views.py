aebd37ac188aee5669c9c774fc2346abff5eb309
andrilor <Andri.lorenz@gmail.com>
2019-05-14 21:39:11 +0000

    búinn að fersta notanda í register ferlinu en er ekki enþá búinn að gefa hönum möguleika á að hætta í miðju ferli

--- castleapartments/user/views.py
+++ castleapartments/user/views.py
@@ -1,21 +1,23 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from user.models import Profile
 from user.forms.profile_form import ProfileForm
-
+from django.contrib.auth import authenticate, login
 
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(data = request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password1'],
+                                    )
+            login(request, new_user)
+            return HttpResponseRedirect("newUserProfile")
     return render(request, 'user/register.html', {
         'form': UserCreationForm()
     })
-
-
-
 
 def profile(request):
     profile = Profile.objects.filter(user=request.user).first()
@@ -28,4 +30,20 @@ def profile(request):
             return redirect('index-mittsvaedi')
     return render(request, 'user/profile.html', {
         'form': ProfileForm(instance=profile)
+    })
+
+def newUser(request):
+    return render(request, 'user/newUserProfile.html', {})
+
+def newUser_upplysingar(request):
+    newUser_upplysingar = Profile.objects.filter(user=request.user).first()
+    if request.method == 'POST':
+        form = ProfileForm(instance=newUser_upplysingar, data=request.POST)
+        if form.is_valid():
+            newUser_upplysingar = form.save(commit=False)
+            newUser_upplysingar.user = request.user
+            newUser_upplysingar.save()
+            return redirect('index-forsida')
+    return render(request, 'user/newUser_upplysingar.html', {
+        'form': ProfileForm(instance=newUser_upplysingar)
     })
