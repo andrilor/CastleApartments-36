ff0f435162bf18da7f19c96aab6c9bc2d34a022f
Fannar <fannarleo95@hotmail.com>
2019-05-15 13:32:36 +0000

    Finished the sorted function for eignir

--- castleapartments/eignir/urls.py
+++ castleapartments/eignir/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = "index-eignir"),
-    path('<int:id>', views.uppl_um_eign, name="eignir")
+    path('<int:id>', views.uppl_um_eign, name="eignir"),
+    path('sorted-name', views.alphebeticallySortedEign, name='sorted-name'),
+    path('sorted-price', views.priceSortedEign, name='sorted-price')
 ]
