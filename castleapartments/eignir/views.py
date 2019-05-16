ad4db8172ffc81e05d20a9ad50f9664d65784cc7
Sigurgeir <sigurgeir16@ru.is>
2019-05-16 23:22:00 +0000

    work towards search functionality

--- castleapartments/eignir/views.py
+++ castleapartments/eignir/views.py
@@ -4,7 +4,46 @@ from opinhus.models import opinhus
 from datetime import datetime
 
 def index(request):
-    context = {'eignir': Eign.objects.all()}
+
+    if request:
+        eignir = Eign.objects.none()
+
+        if 'search' in request.GET:
+            leitar_ord = request.GET['search']
+            eignir += eignir.filter(heimilisfang__icontains=leitar_ord)
+
+        if 'reykjavik' in request.GET:
+            eignir += eignir.filter(baejarfelag__exact='REYKJAVÍK')
+
+        if 'kopavogur' in request.GET:
+            eignir += eignir.filter(baejarfelag__exact='KÓPAVOGUR')
+
+        if 'akranes' in request.GET:
+            eignir += eignir.filter(baejarfelag__exact='AKRANES')
+
+        if 'selfoss' in request.GET:
+            eignir += eignir.filter(baejarfelag__exact='SELFOSS')
+
+        if 'einbyli' in request.GET:
+            eignir += eignir.filter(baejarfelag__exact='Einbýli')
+
+        if 'fjolbyli' in request.GET:
+            eignir += eignir.filter(baejarfelag__exact='Fjölbýli')
+
+        if 'radhus' in request.GET:
+            eignir += eignir.filter(baejarfelag__exact='Raðhús')
+
+        if 'haed' in request.GET:
+            eignir += eignir.filter(baejarfelag__exact='Hæð')
+
+        if 'sumarhus' in request.GET:
+            eignir += eignir.filter(baejarfelag__exact='Sumarhús')
+
+    else:
+        eignir = Eign.objects.all()
+
+
+    context = {'eignir': eignir}
     return render(request, 'eignir/index.html', context)
 
 def uppl_um_eign(request, id):
