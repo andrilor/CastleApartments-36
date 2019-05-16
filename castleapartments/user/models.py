647f65963b65c6bf94812d8540711de2e89ac28e
birgitta run <birgittarun95@gmail.com>
2019-05-16 12:35:06 +0000

    breyta profile modelinu

--- castleapartments/user/models.py
+++ castleapartments/user/models.py
@@ -2,11 +2,17 @@ from django.contrib.auth.models import User
 from django.db import models
 from eignir.models import Eign
 
+class Land(models.Model):
+    nafn = models.CharField(max_length=255)
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     fullt_nafn = models.CharField(max_length=255)
     kennitala = models.IntegerField()
     heimilisfang = models.CharField(max_length=255)
+    borg = models.CharField(max_length=255)
+    land = models.ForeignKey(Land, on_delete=models.CASCADE)
+    postnr = models.IntegerField()
     netfang = models.EmailField(max_length=50)
     simi = models.IntegerField()
     mynd = models.CharField(max_length=9999, blank=True)
