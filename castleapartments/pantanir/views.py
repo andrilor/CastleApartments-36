0c1c57c39573951614827420683f214acf5199ad
birgitta run <birgittarun95@gmail.com>
2019-05-16 12:13:25 +0000

    pantanaferlið eiginlega komið, vantar að gera fínt og taka eignina af skrá

--- castleapartments/pantanir/views.py
+++ castleapartments/pantanir/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from eignir.models import Eign
 
 
-def index(request):
-    return render(request, 'pantanir/index.html')
+
+def stadfesta(request, id):
+    context = {'card': request.POST['cardnumber'],
+               'name': request.POST['cardname'],
+               'expmonth': request.POST['expmonth'],
+               'expyear': request.POST['expyear'],
+               'cvc': request.POST['cvv'],
+               'eign': Eign.objects.get(id=id),
+               }
+
+    return render(request, 'pantanir/pontun_stadfesta.html', context)
 
 
+def index(request, id):
+    context = {'eign': Eign.objects.get(id=id)}
+    return render(request, 'pantanir/index.html', context)
+
+
+
+def kvittun(request,id):
+    context = {'eign': Eign.objects.get(id=id)}
+    return render(request, 'pantanir/kvittun.html', context)
--- castleapartments/user/models.py
+++ castleapartments/user/models.py
@@ -4,9 +4,10 @@ from eignir.models import Eign
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    netfang = models.EmailField(max_length=50, blank=False)
     fullt_nafn = models.CharField(max_length=255)
+    kennitala = models.IntegerField()
     heimilisfang = models.CharField(max_length=255)
+    netfang = models.EmailField(max_length=50)
     simi = models.IntegerField()
     mynd = models.CharField(max_length=9999, blank=True)
 
