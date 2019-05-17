cc02d6bf9b376b0aae423df0ddf1e7eede870282
Sigurgeir <sigurgeir16@ru.is>
2019-05-17 15:00:56 +0000

    search input saved

--- castleapartments/forsida/urls.py
+++ castleapartments/forsida/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from eignir import views as view_eignir
 
 urlpatterns = [
     path('', views.index, name = "index-forsida"),
+    path('', view_eignir.index, name = "index-eignir"),
     path('<int:id>', views.get_eign_by_id, name="eignir"),
     path('<int:id>', views.get_starfsmenn_by_id, name="starfsmenn_upplisingar")
 ]
