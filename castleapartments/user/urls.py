9b5fd7831c39bdefca1d8de7ccd9bd782b7ca3e9
birgitta run <birgittarun95@gmail.com>
2019-05-17 16:36:39 +0000

    gleymt lykilorð virkar næstum því

--- castleapartments/user/urls.py
+++ castleapartments/user/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetConfirmView, PasswordResetCompleteView, PasswordResetDoneView
 from django.urls import path
 from . import views
 
@@ -11,4 +11,8 @@ urlpatterns = [
     path('newUserUpplysingar', views.newUser_upplysingar, name = 'newUserUpplysingar'),
     #path('profile/<username>/', views.logout, name='logout'),
     #url(r'^profile_view/(?P<username>\w+)/$', views.logout, name='logout'),
+    path('password_reset/', PasswordResetView.as_view(template_name='user/gleymt_lykilord.html'),name='password_reset'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(template_name='user/nytt_lykilord.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(template_name='user/password_reset_complete.html'), name='password_reset_complete'),
 ]
